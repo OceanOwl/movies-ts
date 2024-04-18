1603d31a80e610f6f0b3062cf169d292617da005
OceanOwl <115832268+OceanOwl@users.noreply.github.com>
2024-04-18 16:56:52 +0200

    separated trailer component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 
 import {Switch} from "@mui/material";
 import css from './Header.module.css'
